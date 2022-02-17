e5d127d6aff84f5e9e979e64e5f6f53eb6834bd9
Andres Bernal <publiminal@gmail.com>
2022-02-17 01:35:52 -0500

    done

--- index.js
+++ index.js
@@ -39,11 +39,46 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
 
+Person.prototype.eat = function (someFood){
+  const isEdible = someFood || false;
+  const isFull = this.stomach.length >= 10;
+  if(isEdible && !isFull){
+    this.stomach.push(someFood);
+    console.log(`${this.name} is eating food : is edible:${isEdible} - isFull:${isFull} / ${this.stomach.length} `)
+    }else if(isEdible && isFull){
+       console.log(`${this.name} is trying to eat but is full :  isFull:${isFull} - ${this.stomach.length}  `);
+  }else if(!isEdible)
+      console.log(`${this.name} is trying but it is not edible :  isEdible:${isEdible} - ${this.stomach.length}  `);
+}
+Person.prototype.poop = function (){
+  this.stomach = [];
+  console.log(`${this.name} is pooping.`);
+}
 
+Person.prototype.toString = function (){
+   return `${this.name}, ${this.age} `;
+}
+let neo = new Person('Neo',20);
+neo.toString();
+neo.eat('1');
+neo.eat('2');
+neo.eat('3');
+neo.eat('4');
+neo.eat('5');
+neo.eat('6');
+neo.eat('7');
+neo.eat('8');
+neo.eat('9');
+neo.eat('10');
+neo.eat('11');
+neo.eat('12');
+neo.poop();
 
 
 
@@ -63,10 +98,31 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+
+  Car.prototype.fill = function(gallons){
+    this.tank += gallons;
+  }
+
+  Car.prototype.drive = function(distance){
+    this.odometer = distance / this.tank; 
+    const milesAvail = this.tank*this.milesPerGallon;
+    const isOutOfFuel = this.tank >= 0;
+    const canDrive = distance <= milesAvail; 
+    if(!isOutOfFuel && canDrive){
+      this.odometer++;
+      this.tank--;
+    }else if(isOutOfFuel){
+      return `I ran out of fuel at ${this.odometer} ${distance} miles!`
+    }
+  }
 }
 
+const batMobile = new Car('BatMobile', 20);
 
 /*
   TASK 3
@@ -75,18 +131,26 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
- 
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age)
+  this.favoriteToy = favoriteToy;
 }
 
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`;
+}
+
+const bebe = new Baby('andres', 4, 'cars');
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. window / global : main context where all js code is initialized by default.
+  2. Implicit : attached to an the current object of any inner property called by the dot operator.
+  3. Explicit : overwrite the current object's context with another one. 
+  4. New : attached to the object's instances created by the object Constructor.
 */
 
 
